b0710d2cd1d0c1ec9e13411a29e1e31b2822d35b
Fladit <zatsarin77@gmail.com>
2020-07-31 11:54:11 +0300

    main site design

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -24,26 +24,20 @@ const Main = () => {
 
     const changeFirstInputValue = (e) => {
         const value = parseInt(e.target.value);
-        if (value >= secondInputValue) {
-            console.log("no ", value, " ", secondInputValue)
-            //e.preventDefault();
-        }
-        else {
+        if (value < secondInputValue && ((secondInputValue - value) <= 15)) {
             console.log("yes ", value, " ", secondInputValue);
             setFirstInputValue(value);
+            //e.preventDefault();
         }
     };
 
     const changeSecondInputValue = (e) => {
         const value = parseInt(e.target.value);
         //console.log(typeof e.target.value, typeof secondInputValue, "70" > 100,  70 > 100, "70" > "100")
-        if (value <= firstInputValue)
+        if (value > firstInputValue && (value - firstInputValue <= 15))
         {
-            e.preventDefault();
-        }
-        else {
+            console.log("yes", firstInputValue, value);
             setSecondInputValue(value);
-            console.log(firstInputValue + " " + value + "(" + secondInputValue + ")", value, ">", firstInputValue, " is ", value > firstInputValue)
         }
 
     };
@@ -53,14 +47,22 @@ const Main = () => {
             <div className={"main-track"}>Что за трек?</div>
             <div className={"main-insert-title"}>Вставьте ссылку на видео с YouTube</div>
             <input type={"text"} value={link} onChange={changeLink} placeholder={"Введите ссылку на видео...."}/>
-            {checked? <div>
+            {checked? <div className={"main-container"}>
                 {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
                 <iframe className={"main-youtube-player"} width="560" height="315" src="https://www.youtube.com/embed/bWv-bR_X-VM" frameBorder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/></div> : ""}
+                        allowFullScreen/>
+                        <div className={"main-container-text-fields"}>
+                            <div className={"main-container-text-fields-time"}>{"12:15:" + firstInputValue}</div>
+                            <div className={"main-container-text-fields-text"}> Введите промежуток </div>
+                            <div className={"main-container-text-fields-time"}>{"12:15:" + secondInputValue}</div>
+                        </div>
+                <div className={"range-slider"}>
+                    <input type={"range"} min={"0"} max={"100"} step={"1"} value={firstInputValue} onChange={changeFirstInputValue} />
+                    <input type={"range"} min={"0"} max={"100"} step={"1"} value={secondInputValue} onChange={changeSecondInputValue}/>
+                </div>
+                        </div>:""}
             <button onClick={!checked? findVideo : findSong}> Найти </button>
-            <input type={"range"} min={"0"} max={"100"} step={"1"} value={firstInputValue} onChange={changeFirstInputValue} />
-            <input type={"range"} min={"0"} max={"100"} step={"1"} value={secondInputValue} onChange={changeSecondInputValue}/>
         </div>
     );
 };
