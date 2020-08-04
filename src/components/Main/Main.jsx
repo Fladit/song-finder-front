b80cc4e70aac4c5d7fd937b2a92b416f571ea060
Fladit <zatsarin77@gmail.com>
2020-08-04 10:23:06 +0300

    correct displaying of time

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import "./main.css"
 import axios from "axios"
+import getTime from "./time";
 
 const Main = () => {
     const hostURL = "http://localhost:3001"
@@ -8,6 +9,8 @@ const Main = () => {
     const [checked, setChecked] = useState(false)
     const [firstInputValue, setFirstInputValue] = useState(0)
     const [secondInputValue, setSecondInputValue] = useState(1)
+    const [duration, setDuration] = useState(10);
+    const distinction = 100;
     useEffect(() => {
         console.log(firstInputValue, secondInputValue)
     })
@@ -21,7 +24,12 @@ const Main = () => {
             const res = await axios.post(`${hostURL}/duration`, {id: videoUrl});
             console.log(`Duration: ${res.data.duration}`);
             if (res.data.duration > 5)
+            {
+                const newLink = "https://www.youtube.com/embed/" + res.data.videoID;
+                setDuration(res.data.duration);
+                setLink(newLink);
                 setChecked(true);
+            }
         }
         catch (e) {
             throw e;
@@ -34,8 +42,8 @@ const Main = () => {
 
     const changeFirstInputValue = (e) => {
         const value = parseInt(e.target.value);
-        if (value < secondInputValue && ((secondInputValue - value) <= 15)) {
-            console.log("yes ", value, " ", secondInputValue);
+        if (value < secondInputValue && ((secondInputValue - value) <= distinction)) {
+            //console.log("yes ", value, " ", secondInputValue);
             setFirstInputValue(value);
             //e.preventDefault();
         }
@@ -44,16 +52,16 @@ const Main = () => {
     const changeSecondInputValue = (e) => {
         const value = parseInt(e.target.value);
         //console.log(typeof e.target.value, typeof secondInputValue, "70" > 100,  70 > 100, "70" > "100")
-        if (value > firstInputValue && (value - firstInputValue <= 15))
+        if (value > firstInputValue && (value - firstInputValue <= distinction))
         {
-            console.log("yes", firstInputValue, value);
+            //console.log("yes", firstInputValue, value);
             setSecondInputValue(value);
         }
 
     };
 
     return (
-        <div className={"main"}>
+        <div className={"main"} style={!checked? {marginTop: "30vh"}: {marginTop: 35}}>
             <div className={"main-track"}>Что за трек?</div>
             <div className={"main-insert-title"}>Вставьте ссылку на видео с YouTube</div>
             <input type={"text"} maxLength={70} className={"main-input-link"}  value={link} onChange={changeLink} placeholder={"Введите ссылку на видео...."}/>
@@ -63,13 +71,13 @@ const Main = () => {
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen/>
                         <div className={"main-container-text-fields"}>
-                            <div className={"main-container-text-fields-time"}>{firstInputValue}</div>
+                            <div className={"main-container-text-fields-time"}>{getTime(firstInputValue)}</div>
                             <div className={"main-container-text-fields-text"}> Введите промежуток </div>
-                            <div className={"main-container-text-fields-time"}>{secondInputValue}</div>
+                            <div className={"main-container-text-fields-time"}>{getTime(secondInputValue)}</div>
                         </div>
                 <div className={"range-slider"}>
-                    <input type={"range"} min={"0"} max={"100"} step={"1"} value={firstInputValue} onChange={changeFirstInputValue} />
-                    <input type={"range"} min={"0"} max={"100"} step={"1"} value={secondInputValue} onChange={changeSecondInputValue}/>
+                    <input type={"range"} min={"0"} max={duration} step={"1"} value={firstInputValue} onChange={changeFirstInputValue} />
+                    <input type={"range"} min={"0"} max={duration} step={"1"} value={secondInputValue} onChange={changeSecondInputValue}/>
                 </div>
                         </div>:""}
             <button onClick={ () => !checked? findVideo(link) : findSong(link)}> Найти </button>
---
+++ src/components/Main/time.js
@@ -0,0 +1,9 @@
+const getTime = (time) => {
+    const seconds = time % 60;
+    let remainingTime = time - seconds;
+    const minutes = (remainingTime % 3600) / 60;
+    const totalTime = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+    console.log("min:", minutes, "sec:", seconds, "test:", remainingTime, "a:", (remainingTime % 3600), "time:", time)
+    return totalTime;
+}
+export default getTime;
